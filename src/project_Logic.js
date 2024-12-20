ed0d3e8712ce4e445c19a50a423de788f131f403
Langdon Douglas <langdon.douglas26@gmail.com>
2024-12-19 19:07:44 -0600

    add storeToDo function

--- src/project_Logic.js
+++ src/project_Logic.js
@@ -1,4 +1,4 @@
-import { retrieveTodo } from "./createToDo_Logic";
+import { toDo } from "./createToDo_Logic.js";
 
 export class Project {
     constructor(projectName){
@@ -6,32 +6,42 @@ export class Project {
         this.todos = [];
     };
 
-    //add toDo function
+    //add toDo 
     addToDo(todo) {
         //checking if todo is a todo by checking prototype
         if(todo instanceof toDo) {
             this.todos.push(todo);
-            console.log(`Add ${todo.title} to ${this.name}`);
+            console.log(`Added ${todo.title} to ${this.name}`);
         } else { 
             console.error("Invalid object");
         }
     };
 
-    //remove toDo function
-    removeTodDo(todoTitle){
-        this.todos = this.todos.filter(todo => todo.title !== todoTitle);
-    }
+    //display toDo
+    storeTodo(){
+        console.log(`Project: "${this.name}"`);
+        
+        //check to see if array is empty if so throw error, if not diplayInfo of toDo
+        if(this.todos.length === 0){
+            console.error("There is no Todos in this project");
+        } else {
+            this.todos.forEach(todo => todo.displayInfo());
+        };
+
+    };
+
+    create
+
+    // //remove toDo function
+    // removeTodDo(todoTitle){
+    //     this.todos = this.todos.filter(todo => todo.title !== todoTitle);
+    // }
 
     //extendTodo(todo){
 
-    //display toDo information 
+
 
 
 } 
 
-const testProject = new Project("Test");
-
-const testAdd = new toDo("Test", "test", "test", "test", "test", "test");
-
-testProject.addToDo(testAdd);
 
