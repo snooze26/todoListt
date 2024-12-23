83aa4b7cf949d4c639e080c10f415f577ec1f75f
Langdon Douglas <langdon.douglas26@gmail.com>
2024-12-23 07:03:51 -0600

    Add form and linked values to retrieve todo form

--- src/createToDo_Logic.js
+++ src/createToDo_Logic.js
@@ -18,25 +18,26 @@ export class toDo {
 }
 
 export function retrieveTodo() {
-    const title = prompt("Enter the title:");
+    
+    const title = document.querySelector("#title").value;
     if(!title) return null;
 
-    const description = prompt("Enter the description");
+    const description = document.querySelector("#description").value;
     if(!description) return null;
 
-    const dueDate = prompt("When is this due?");
+    const dueDate = document.querySelector("#dueDate").value;
     if(!dueDate) return null;
     
-    const priority = prompt("Rate the level of priority: High, Medium, Low");
+    const priority = document.querySelector("#priority").value;
     if(!priority) return (null);
 
-    const notes = prompt("Insert any notes");
+    const notes = document.querySelector("#notes").value;
     if(!notes) return (null);
 
     const now = new Date();
     const timeStamp = format(now, "yyyy-MM-dd HH:mm");
 
-    console.log(timeStamp);
+    console.log(title);
 
     return new toDo(title, description, dueDate, priority, notes, timeStamp);
 }
